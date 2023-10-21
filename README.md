498a03c1f318e9d17b0ec37a52878695f58ffd7c
SweetId <2630750+SweetId@users.noreply.github.com>
2023-10-20 20:23:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# xmake-repo
+XMake-repo containing latest Nazara Localization packages for xmake, for testing before integration on https://github.com/xmake-io/xmake-repo
---
+++ packages/n/nazaralocalization/xmake.lua
@@ -0,0 +1,18 @@
+package("nazaralocalization")
+
+	set_kind("library")
+	set_homepage("https://github.com/NazaraEngine")
+	set_description("Localization package for Nazara projects")
+	set_license("MIT")
+
+	add_urls("https://github.com/SweetId/NazaraLocalization.git")
+
+	add_versions("2023.10.20", "0b747c4d0a34df82b93717f129512da2e0936d89")
+	
+	add_deps("nazaraengine")
+	add_deps("fmt")
+
+	on_install(function (package)
+		import("package.tools.xmake").install(package)
+	end)
+
